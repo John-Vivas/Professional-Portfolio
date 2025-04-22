0c9b1228905cc768b88dd0c324b61a50f8b823d7
john vivas <john0122vivas98@gmail.com>
2025-04-22 13:08:51 -0500

    organize jaso and config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://John-vivas.github.io/Professional-Portfolio',
+  base: 'https://John-vivas.github.io/Professional-Portfolio/',
 })
