9573392b9901910b306892a17d14010ad8189623
john vivas <john0122vivas98@gmail.com>
2025-04-22 13:51:14 -0500

    changes routes img

--- frontend/src/components/home/Hero.tsx
+++ frontend/src/components/home/Hero.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { ArrowRight, Download } from 'lucide-react';
 import Button from '../shared/Button';
 import { textVariant, fadeIn } from '../../utils/animations';
-import profilePhoto from '../../assets/img/professional_portfolio.png';
+import profilePhoto from '../../assets/img/photo_profetional.jpg';
 
 const Hero: React.FC = () => {
   return (
